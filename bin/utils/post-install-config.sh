0c563ac63b34344538f5aadc38f9e67e8f12c162
myhau <mifud3@gmail.com>
2014-12-23 14:01:55 +0100

    bin now

---
+++ bin/utils/post-install-config.sh
@@ -0,0 +1,29 @@
+# shortcuts
+
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/Super_L" -s "xfce4-popup-whiskermenu"
+
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Super>Left" -s tile_left_key
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Super>Down" -s tile_down_key
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Super>Right" -s tile_right_key
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Super>Up" -s tile_up_key
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Super>9" -s maximize_window_key
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Super>0" -s hide_window_key
+
+
+# themes / look n feel
+
+#theme
+xfconf-query -c xfwm4 -p /general/theme -s Greybird-compact
+
+#icon theme
+xfconf-query -c xsettings -p /Net/IconThemeName -s Numix-Circle
+
+#font 
+xfconf-query -c xsettings -p /Gtk/FontName -s "Droid Sans 9"
+
+#tranparent panel
+xfconf-query -c xfce4-panel -p /panels/panel-0/background-alpha -s 0 
+
+#colorzz
+xfconf-query -c xsettings -p /Gtk/ColorScheme -s "selected_bg_color:#3465a4;selected_fg_color:#eeeeee;"
+
---
+++ bin/utils/post-install.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+## xubuntu post-installation script
+
+echo "Downloading GetDeb and PlayDeb" &&
+wget http://archive.getdeb.net/install_deb/getdeb-repository_0.1-1~getdeb1_all.deb http://archive.getdeb.net/install_deb/playdeb_0.3-1~getdeb1_all.deb &&
+
+echo "Installing GetDeb" &&
+sudo dpkg -i getdeb-repository_0.1-1~getdeb1_all.deb &&
+
+echo "Installing PlayDeb" &&
+sudo dpkg -i playdeb_0.3-1~getdeb1_all.deb &&
+
+echo "Deleting Downloads" &&
+rm -f getdeb-repository_0.1-1~getdeb1_all.deb &&
+rm -f playdeb_0.3-1~getdeb1_all.deb
+
+# vlc repo
+sudo add-apt-repository -y ppa:videolan/stable-daily
+
+# gimp repo
+sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
+
+#transmissionbt repo
+sudo add-apt-repository -y ppa:transmissionbt/ppa
+
+# numix theme repo
+sudo apt-add-repository -y ppa:numix/ppa
+
+
+sudo apt-get update
+
+#uninstall bullshit
+
+
+# essential toooooolz
+sudo apt-get -y install vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager openjdk-8-jre openjdk-8-jdk unace unrar zip unzip p7zip-full p7zip-rar sharutils rar arj cabextract libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras libreoffice qalculator maxima virtualbox irssi iotop sysstat baobab gparted xclip vim aria2 usb-creator-gtk numix-icon-theme-circle
+
+
+
+# install chrome beta
+if [[ $(getconf LONG_BIT) = "64" ]]
+then
+	echo "64bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb &&
+	sudo dpkg -i google-chrome-beta_current_amd64.deb &&
+	rm -f google-chrome-beta_current_amd64.deb
+else
+	echo "32bit Detected" &&
+	echo "Installing Google Chrome" &&
+	wget https://dl.google.com/linux/direct/google-chrome-beta_current_i386.deb &&
+	sudo dpkg -i google-chrome-beta_current_i386.deb &&
+	rm -f google-chrome-beta_current_i386.deb
+fi
+
+
+# upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+
+
+# cleanup
+
+
+sudo apt-get -f install &&
+sudo apt-get -y autoremove &&
+sudo apt-get -y autoclean &&
+sudo apt-get -y clean
+
+
+./post-install-config.sh
